55b487b2a7e7934deeef1e02e6e1d95250239f1a
ctle-vn <chriiisle@gmail.com>
2022-06-12 20:45:57 -0700

    Added 2nd migration script.

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,6 +1,44 @@
 // migrations/2_deploy.js
 const StreamScheduler = artifacts.require("StreamScheduler");
+const ConstantFlowAgreement = artifacts.require("IConstantFlowAgreementV1");
+const Superfluid = artifacts.require("ISuperfluid");
+// import deployFramework from "@superfluid-finance/ethereum-contracts/scripts/deploy-framework";
+// import deployTestToken from "@superfluid-finance/ethereum-contracts/scripts/deploy-test-token";
+// import deploySuperToken from "@superfluid-finance/ethereum-contracts/scripts/deploy-super-token";
+
+// export const errorHandler = (type, err) => {
+//     if (err) console.error("Deploy " + type + " Error: ", err);
+// };
+
+// export async function deployFrameworkAndTokens() {
+//     try {
+//         const [Deployer] = (await ethers.getSigners()).map(x => x.address);
+//         await deployFramework(x => errorHandler("Framework", x), {
+//             web3: global.web3,
+//             from: Deployer,
+//         });
+//         await deployTestToken(
+//             x => errorHandler("TestToken", x),
+//             [":", "fDAI"],
+//             {
+//                 web3: global.web3,
+//                 from: Deployer,
+//             },
+//         );
+//         await deploySuperToken(
+//             x => errorHandler("SuperToken", x),
+//             [":", "fDAI"],
+//             {
+//                 web3: global.web3,
+//                 from: Deployer,
+//             },
+//         );
+//     } catch (err) {
+//         console.error(err);
+//     }
+// }
 
 module.exports = async function (deployer) {
-    await deployer.deploy(Box);
+    // deployFrameworkAndTokens();
+    await deployer.deploy(StreamScheduler, ConstantFlowAgreement, Superfluid);
 };
