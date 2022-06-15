53fb7713834895f65ff7a4f3f55858d33baffcf1
ctle-vn <chriiisle@gmail.com>
2022-06-15 09:48:06 -0700

    Added bot execute logic with db reads.

--- database/init.sql
+++ database/init.sql
@@ -2,30 +2,60 @@ BEGIN;
 
 -- initialise your tables here
 -- insert some test data if you like
+DROP TABLE stream_orders;
 
 CREATE TABLE IF NOT EXISTS stream_orders (
     id serial PRIMARY KEY,
    event_name varchar(100) NOT NULL,
    event_receiver varchar(100) NOT NULL,
    event_sender varchar(100) NOT NULL,
-   event_data_hash varchar(100),
-   event_block_number varchar(100),
-   event_timestamp varchar(100),
+   event_block_number bigint NOT NULL,
+   event_flow_rate numeric(30,0) NOT NULL, -- int96
+   event_end_time numeric(78,0) NOT NULL, -- uint256
+   event_start_time numeric(78,0) NOT NULL, -- uint256
    event_super_token varchar(100),
-   event_flow_rate varchar(100),
-   event_end_time varchar(100),
-   event_user_data varchar(100) 
+   event_user_data varchar(100),
+   is_active boolean NOT NULL DEFAULT true 
 );
 
 -- Insert some test data into stream_orders
 INSERT INTO stream_orders (
     event_name,
     event_receiver,
-    event_sender
+    event_sender,
+    event_block_number,
+    event_flow_rate,
+    event_end_time,
+    event_start_time
 ) VALUES (
     'test',
     'fake_receiver',
-    'fake_senderroo'
+    'fake_senderroo',
+    0,
+    1000000000000,
+    1656266815,
+    1655267815
 );
+
+INSERT INTO stream_orders (
+    event_name,
+    event_receiver,
+    event_sender,
+    event_block_number,
+    event_flow_rate,
+    event_end_time,
+    event_start_time
+) VALUES (
+    'test',
+    'fake_receiver',
+    'fake_senderroo',
+    2,
+    1000000000000,
+    1656276815,
+    1655267815
+);
+
+-- Query table for the latest block number
+-- SELECT event_block_number FROM stream_orders ORDER BY event_block_number DESC LIMIT 1;
 
 COMMIT;
--- scripts/index.js
+++ scripts/index.js
@@ -12,14 +12,16 @@ const SuperfluidABI = SuperfluidJSON.abi;
 const pg = require("pg");
 const path = require("path");
 const fs = require("fs");
+const connectionString =
+    "postgres://superfluid:password@localhost:5432/superfluid";
 const sql = fs
     .readFileSync(path.resolve(__dirname, "./../database/init.sql"))
     .toString();
-// Write sql to postgres database
+
+// Init.sql to create table and seed data
 const writeSQL = async () => {
     const client = new pg.Client({
-        connectionString:
-            "postgres://superfluid:password@localhost:5432/superfluid",
+        connectionString,
     });
     await client.connect();
     await client.query(sql);
@@ -40,18 +42,50 @@ const fDaiAddress = "0x1f65B7b9b3ADB4354fF76fD0582bB6b0d046a41c";
 
 const flowRate = "1000000000000";
 
+/**
+ * Parse to rows to get format:
+ *   {
+    event_name: string,
+    event_receiver: string,
+    event_sender: string,
+    event_block_number: int,
+    event_flow_rate: int96,
+    event_end_time: bigint,
+    event_start_time: uint256,
+    event_data_hash: string,
+    event_super_token: string,
+    event_user_data: string,
+  }
+ */
+const parseToStreamOrder = rows => {
+    let streamOrders = [];
+    for (let i = 0; i < rows.length; i++) {
+        streamOrders.push({
+            name: rows[i].event_name,
+            receiver: rows[i].event_receiver,
+            sender: rows[i].event_sender,
+            blockNumber: parseInt(rows[i].event_block_number),
+            flowRate: parseInt(rows[i].event_flow_rate),
+            endTime: parseInt(rows[i].event_end_time),
+            startTime: parseInt(rows[i].event_start_time),
+            superToken: rows[i].event_super_token,
+            userData: rows[i].event_user_data,
+        });
+    }
+    return streamOrders;
+};
+
 async function main() {
-    console.log("================ CREATING TABLE =================");
-    await writeSQL();
-    console.log("================ DONE CREATING =================");
+    // console.log("================ CREATING TABLE =================");
+    // await writeSQL();
+    // console.log("================ DONE CREATING =================");
     console.log("================ START QUERY =================");
     const client = new pg.Client({
-        connectionString:
-            "postgres://superfluid:password@localhost:5432/superfluid",
+        connectionString,
     });
     await client.connect();
     const result = await client.query("SELECT * FROM stream_orders;");
-    console.log("ROWS: ", result.rows);
+    console.log("ROWS: ", parseToStreamOrder(result.rows));
     console.log("================ DONE QUERY =================");
 
     const StreamScheduler = await ethers.getContractFactory("StreamScheduler");
@@ -86,6 +120,7 @@ async function main() {
     const startTime1 = Math.floor(Date.now() / 1000) + 1000;
     const endTime1 = Math.floor(Date.now() / 1000) + 1000000;
     console.log("================ CREATE STREAM ORDER =================");
+    // New stream order
     await streamScheduler.createStreamOrder(
         accounts[1],
         fDaiAddress,
@@ -95,15 +130,39 @@ async function main() {
         endTime1,
         "0x",
     );
-    await streamScheduler.createStreamOrder(
-        accounts[1],
-        fDaiAddress,
-        // Convert Date.now to seconds
-        Math.floor(Date.now() / 1000) + 1000,
-        flowRate,
-        Math.floor(Date.now() / 1000) + 10000230,
-        "0x",
-    );
+
+    // Duplicate stream order but should update with new flow rate.
+    // await streamScheduler.createStreamOrder(
+    //     accounts[1],
+    //     fDaiAddress,
+    //     // Convert Date.now to seconds
+    //     Math.floor(Date.now() / 1000) + 1000,
+    //     flowRate,
+    //     Math.floor(Date.now() / 1000) + 10000230,
+    //     "0x",
+    // );
+
+    // // Stream order with no close time
+    // await streamScheduler.createStreamOrder(
+    //     accounts[1],
+    //     fDaiAddress,
+    //     // Convert Date.now to seconds
+    //     Math.floor(Date.now() / 1000) + 1000,
+    //     flowRate,
+    //     0,
+    //     "0x",
+    // );
+
+    // // Stream order with no close time
+    // await streamScheduler.createStreamOrder(
+    //     accounts[1],
+    //     fDaiAddress,
+    //     // Convert Date.now to seconds
+    //     Math.floor(Date.now() / 1000) + 1000,
+    //     flowRate,
+    //     0,
+    //     "0x",
+    // );
     console.log(
         "Length of stream order hashes: ",
         await streamScheduler.getStreamOrderHashesLength(),
@@ -125,12 +184,10 @@ async function main() {
         const eventData = event.args;
         const streamOrderData = parseEventDataArgs(eventData);
         const eventName = event.event;
-        const eventDataHash = event.data;
         const eventBlockNumber = event.blockNumber;
         const eventTimestamp = event.timestamp;
         const blob = {
             event_name: eventName,
-            event_data_hash: eventDataHash,
             event_block_number: eventBlockNumber,
             event_timestamp: eventTimestamp,
             event_receiver: streamOrderData.receiver,
@@ -144,26 +201,42 @@ async function main() {
         latestBlock = eventBlockNumber;
     }
 
-    console.log("================ EXECUTE CREATE STREAM =================");
+    // console.log("================ EXECUTE CREATE STREAM =================");
 
-    // Call executeCreateStream
-    await streamScheduler.executeCreateStream(
-        accounts[1],
-        fDaiAddress,
-        // Convert Date.now to seconds
-        startTime1,
-        flowRate,
-        endTime1,
-        "0x",
-    );
+    // // Call executeCreateStream
+    // await streamScheduler.executeCreateStream(
+    //     accounts[1],
+    //     fDaiAddress,
+    //     // Convert Date.now to seconds
+    //     startTime1,
+    //     flowRate,
+    //     endTime1,
+    //     "0x",
+    // );
 
     // get past events emitted from contract
-    events = await streamScheduler.queryFilter(
-        streamScheduler.filters.ExecuteCreateStream(),
-        latestBlock,
-        "latest",
+    // events = await streamScheduler.queryFilter(
+    //     streamScheduler.filters.ExecuteCreateStream(),
+    //     latestBlock,
+    //     "latest",
+    // );
+    // console.log("Events:", events);
+    /*
+    msg.sender,
+                    receiver,
+                    superToken,
+                    startTime,
+                    endTime
+    */
+    console.log(
+        "Contract state variable: ",
+        await streamScheduler.streamOrderHashes(
+            ethers.utils.solidityKeccak256(
+                ["address", "address", "address", "uint256", "uint256"],
+                [accounts[0], accounts[1], fDaiAddress, startTime1, endTime1],
+            ),
+        ),
     );
-    console.log("Events:", events);
 }
 
 const errorHandler = (type, err) => {
@@ -172,14 +245,235 @@ const errorHandler = (type, err) => {
 
 const parseEventDataArgs = eventData => {
     const streamOrderData = {};
-    streamOrderData.streamOrderHash = eventData.receiver;
+    streamOrderData.receiver = eventData.receiver;
     streamOrderData.sender = eventData.sender;
-    streamOrderData.sender = eventData.superToken;
-    streamOrderData.sender = eventData.flowRate;
-    streamOrderData.sender = eventData.endTime;
-    streamOrderData.sender = eventData.userData;
+    streamOrderData.superToken = eventData.superToken;
+    streamOrderData.flowRate = eventData.flowRate;
+    streamOrderData.endTime = eventData.endTime;
+    streamOrderData.userData = eventData.userData;
     return streamOrderData;
 };
+
+async function runBot(events) {
+    // Query the db for the latest block number.
+    const latestBlockNumber = await getLatestBlockNumberFromDB();
+    console.log("Latest Block Number: ", latestBlockNumber);
+
+    // Use this block number as the "from" parameter and get all past events from the contract.
+    const pastEvents = await getPastEventsFromContract(latestBlockNumber);
+
+    // Store all the stream orders back into the database.
+    await storeStreamOrdersIntoDB(pastEvents);
+
+    // Loop through the events and check differentiate the diff types of stream orders.
+    await processStreamOrders(pastEvents);
+}
+
+async function processStreamOrders(events) {
+    const timeNowInSecs = Math.floor(Date.now() / 1000);
+    let streamOrdersToDelete = [];
+    let streamOrdersToUpdateOrCreate = [];
+    for (let i = 0; i < events.length; i++) {
+        if (events[i].endTime != 0 && events[i].endTime < timeNowInSecs) {
+            console.log("Detected close stream order");
+            await streamScheduler.executeDeleteStream(
+                events[i].receiver,
+                events[i].superToken,
+                events[i].startTime,
+                events[i].flowRate,
+                events[i].endTime,
+                events[i].userData,
+            );
+            streamOrdersToDelete.push({
+                name: events[i].event_name,
+                receiver: events[i].event_receiver,
+                sender: events[i].event_sender,
+                blockNumber: parseInt(events[i].event_block_number),
+                flowRate: parseInt(events[i].event_flow_rate),
+                endTime: parseInt(events[i].event_end_time),
+                startTime: parseInt(events[i].event_start_time),
+                superToken: events[i].event_super_token,
+                userData: events[i].event_user_data,
+            });
+        } else {
+            streamOrdersToUpdateOrCreate.push({
+                name: events[i].event_name,
+                receiver: events[i].event_receiver,
+                sender: events[i].event_sender,
+                blockNumber: parseInt(events[i].event_block_number),
+                flowRate: parseInt(events[i].event_flow_rate),
+                endTime: parseInt(events[i].event_end_time),
+                startTime: parseInt(events[i].event_start_time),
+                superToken: events[i].event_super_token,
+                userData: events[i].event_user_data,
+            });
+        }
+    }
+
+    for (let i = 0; i < streamOrdersToUpdateOrCreate.length; i++) {
+        let streamOrder = streamOrdersToUpdateOrCreate[i];
+        const shouldCreateStreamOrder = await checkIfStreamOrderExistsInDB(
+            streamOrder,
+        );
+        if (shouldCreateStreamOrder) {
+            await streamScheduler.executeCreateStream(
+                streamOrder.receiver,
+                streamOrder.superToken,
+                streamOrder.startTime,
+                streamOrder.flowRate,
+                streamOrder.endTime,
+                streamOrder.userData,
+            );
+        } else {
+            await streamScheduler.executeUpdateStream(
+                streamOrder.receiver,
+                streamOrder.superToken,
+                streamOrder.startTime,
+                streamOrder.flowRate,
+                streamOrder.endTime,
+                streamOrder.userData,
+            );
+            // Update stream order in database.
+            await updateStreamOrderInDB(streamOrder);
+        }
+    }
+
+    // Delete stream orders that have been closed from the database.
+    streamOrdersToDelete.push(await getExpiredStreamOrdersFromDB());
+    await deleteStreamOrdersFromDB(streamOrdersToDelete);
+}
+
+async function getExpiredStreamOrdersFromDB() {
+    const timeNowInSecs = Math.floor(Date.now() / 1000);
+    const client = new pg.Client({
+        connectionString: connectionString,
+    });
+    await client.connect();
+    const sql = `SELECT event_name, 
+    event_data_hash,
+     event_block_number, 
+     event_timestamp, 
+     event_receiver, 
+     event_sender, 
+     event_super_token, 
+     event_flow_rate, 
+     event_end_time, 
+     event_user_data
+    FROM stream_orders WHERE event_end_time < $1`;
+    const values = [timeNowInSecs];
+    const result = await client.query(sql, values);
+    return parseToStreamOrder(result.rows);
+}
+
+async function updateStreamOrderInDB(streamOrder) {
+    const sql = `UPDATE stream_orders SET
+        event_flow_rate = ?,
+        WHERE event_sender = ? AND event_receiver = ?`;
+    const values = [
+        streamOrder.flowRate,
+        streamOrder.sender,
+        streamOrder.receiver,
+    ];
+    await db.run(sql, values);
+    console.log("Updated stream order in database");
+}
+
+async function deleteStreamOrdersFromDB(streamOrdersToDelete) {
+    const client = new pg.Client({
+        connectionString: connectionString,
+    });
+    await client.connect();
+    for (let i = 0; i < streamOrdersToDelete.length; i++) {
+        let streamOrder = streamOrdersToDelete[i];
+        // TODO: Batch delete from db.
+        const deleteQuery = `DELETE FROM stream_orders WHERE event_receiver = '${streamOrder.receiver}' AND event_sender = '${streamOrder.sender}'`;
+        await client.query(deleteQuery);
+    }
+    await client.end();
+    console.log("Deleted stream orders from database");
+}
+
+async function checkIfStreamOrderExistsInDB(streamOrder) {
+    const client = new pg.Client({
+        connectionString: connectionString,
+    });
+    await client.connect();
+    const sql = `select count(*) from stream_orders 
+    where stream_order.receiver = event_receiver
+    and stream_order.sender = event_sender`;
+    const result = await client.query(sql);
+    return result == 0 ? true : false;
+}
+
+async function insertStreamOrdersIntoDB(streamOrderList) {
+    const client = new pg.Client({
+        connectionString: connectionString,
+    });
+    await client.connect();
+    const sql = `INSERT INTO stream_order (
+        event_name,
+        event_data_hash,
+        event_block_number,
+        event_timestamp,
+        event_receiver,
+        event_sender,
+        event_super_token,
+        event_flow_rate,
+        event_end_time,
+        event_user_data
+    ) VALUES ?`;
+    const values = [streamOrderList];
+    const result = await db.query(sql, [values]);
+    console.log("Inserted stream orders into DB: ", result);
+}
+
+async function storeStreamOrdersIntoDB(events) {
+    let streamOrderList = [];
+    for (let i = 0; i < events.length; i++) {
+        const event = events[i];
+        const eventData = event.args;
+        const streamOrderData = parseEventDataArgs(eventData);
+        const eventName = event.event;
+        const eventBlockNumber = event.blockNumber;
+        const eventTimestamp = event.timestamp;
+        const blob = {
+            event_name: eventName,
+            event_block_number: eventBlockNumber,
+            event_timestamp: eventTimestamp,
+            event_receiver: streamOrderData.receiver,
+            event_sender: streamOrderData.sender,
+            event_super_token: streamOrderData.superToken,
+            event_flow_rate: streamOrderData.flowRate,
+            event_end_time: streamOrderData.endTime,
+            event_user_data: streamOrderData.userData,
+        };
+        streamOrderList.push(blob);
+    }
+    // Insert the stream orders into the database.
+    await insertStreamOrdersIntoDB(streamOrderList);
+}
+
+async function getPastEventsFromContract(latestBlockNumber) {
+    return await streamScheduler.queryFilter(
+        streamScheduler.filters.CreateStreamOrder(),
+        latestBlockNumber,
+        "latest",
+    );
+}
+
+async function getLatestBlockNumberFromDB() {
+    const client = new pg.Client({
+        connectionString: connectionString,
+    });
+    await client.connect();
+    const sql = `SELECT max(event_block_number) as latest_block_number FROM stream_orders`;
+    if (result.rowCount == 0) {
+        console.log("No data in the database, returning default block of 0.");
+        return 0;
+    }
+    console.log("Found data in the database, returning latest block.");
+    return parseInt(result.rows[0].event_block_number);
+}
 
 main()
     .then(() => process.exit(0))
